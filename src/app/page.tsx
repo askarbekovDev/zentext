d866779e9296ce75534faea37f928831de9fe35d
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-01-19 14:53:51 +0600

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,22 @@ import Filter from '@/ui/filter';
 import Status from '../ui/status/index';
 import SearchBar from '@/ui/searchBar';
 import Intro from '@/components/intro.tsx';
+import AboutProject from '@/components/aboutProject';
 
 export default function Home() {
   return (
-    // <div>
-    // <Filter />
-    //   <br />
-    //   <Status state="new" /><br /><Status state="approved" /><br /><Status state="rejected" />
-    //   <br />
-    // </div>
-    <Intro />
+    <div>
+      {/* <Filter />
+      <br />
+      <Status state="new" />
+      <br />
+      <Status state="approved" />
+      <br />
+      <Status state="rejected" />
+      <br />
+      <SearchBar/> */}
+      <Intro />
+      <AboutProject />
+    </div>
   );
 }
--- src/components/aboutProject/index.tsx
+++ src/components/aboutProject/index.tsx
@@ -1,7 +1,29 @@
 import React, { FC } from 'react';
+import styles from './index.module.scss';
+import Image from 'next/image';
 
 const AboutProject: FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.abt}>
+      <Image
+        className={styles.turned}
+        alt="img"
+        src="./images/turned-img.svg"
+        width={1000}
+        height={1000}
+      />
+      <div className={styles.description}>
+        <h2>О проекте</h2>
+        <h4>
+          Наша платформа соединяет швейные фабрики и заказчиков, предлагая
+          удобный способ для поиска фабрик, размещения заказов и оптовых покупок
+          одежды. Мы предоставляем простой и надежный инструмент для
+          эффективного взаимодействия между производителями и покупателями,
+          ускоряя процессы и упрощая коммуникацию.
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default AboutProject;
