2d7c54246f8cf30a2f42dc7bd9c18578bc0e2ee2
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-01-21 17:44:24 +0600

    contuct us block is completed, full adaptive

--- src/components/contact-us/index.tsx
+++ src/components/contact-us/index.tsx
@@ -6,19 +6,21 @@ const ContactUs: FC = () => {
   return (
     <div className="container">
       <div className={styles.content}>
-        <Image
-          className={styles.backgroundImg}
-          alt="bgr"
-          src="/images/background.jpeg"
-          width={1280}
-          height={853}
-        />
         <div className={styles.contactUs}>
           <h1>Готовы начать?</h1>
           <h3>
             Свяжитесь с нами и найдите идеальную фабрику для вашего бизнеса!
           </h3>
           <button>from UI</button>
+        </div>
+        <div className={styles.imgWrapper}>
+          <Image
+            className={styles.backgroundImg}
+            alt="bgr"
+            src="/images/background.jpeg"
+            width={1280}
+            height={853}
+          />
         </div>
       </div>
     </div>
