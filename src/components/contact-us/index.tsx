14d06a8a6f3bcc89035f00eaac72bc15a70931da
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-01-21 14:14:40 +0600

    processing

--- src/components/contact-us/index.tsx
+++ src/components/contact-us/index.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
 import React, { FC } from 'react';
+import styles from './index.module.scss';
 
 const ContactUs: FC = () => {
-  return <div>sdtyuioknwcascvsbebwfbekugekbehbqwkj</div>;
+  return (
+    <div className="container">
+      <div className={styles.content}>
+        <Image
+          className={styles.backgroundImg}
+          alt="bgr"
+          src="/images/background.jpeg"
+          width={1280}
+          height={853}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
